af8bbd51a9781f8baccb22b002c45362ea616996
Achille Aubin <ts.safidiniaina@gmail.com>
2026-06-16 04:38:10 +0200

    TestServlet

--- src/com/sprint0/TestServlet.java
+++ src/com/sprint0/TestServlet.java
@@ -1,1 +1,41 @@
-rtuyhkj
+package com.sprint0;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        String url = request.getRequestURL().toString();
+        String message = Utilitaire.message(url);
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>Sprint 0</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Sprint 0 OK</h1>");
+        out.println("<p>" + message + "</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
