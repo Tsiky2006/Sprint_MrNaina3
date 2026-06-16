b9988b65ef8ae0247abb4e765f15950fcb11bfd9
Achille Aubin <ts.safidiniaina@gmail.com>
2026-06-16 03:47:33 +0200

    utilitaire.java

--- src/com/sprint0/Utilitaire.java
+++ src/com/sprint0/Utilitaire.java
@@ -1,8 +1,8 @@
 package com.sprint0;
 
 public class Utilitaire {
-    public static String salut(String nom) {
-        if (nom == null || nom.isEmpty()) return "Bonjour";
-        return "Bonjour, " + nom + "!";
+
+    public static String message(String url) {
+        return "URL appelée : " + url;
     }
 }
